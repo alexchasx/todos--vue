8dd835cd2b7468fe9ea790bfb7df568e3e024c8d
Alexander Chasovnikov <a.s.chasovnikov@yandex.ru>
2023-09-26 12:59:42 +0300

    created AppTodoItem; updated AppTodoList

---
+++ src/components/AppTodoItem.vue
@@ -0,0 +1,30 @@
+<script lang="ts">
+import { PropType, defineComponent } from 'vue';
+
+export default defineComponent({
+  props: {
+    item: {
+      type: Object as PropType<{
+        id: number;
+        text: string;
+        status: boolean;
+      }>,
+      required: true,
+    },
+  },
+});
+</script>
+
+<template>
+  <li class="todo-item" :class="{ 'todo-item--done': item.status }">
+    <div class="todo-item__status">
+      <i class="bi bi-check2"></i>
+    </div>
+
+    <span class="todo-item__text">{{ item.text }}</span>
+
+    <button class="todo-item__remove-button">
+      <i class="bi bi-trash3"></i>
+    </button>
+  </li>
+</template>
--- src/components/AppTodoList.vue
+++ src/components/AppTodoList.vue
@@ -1,31 +1,34 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+import AppTodoItem from '@/components/AppTodoItem.vue';
+
+interface State {
+  items: {
+    id: number;
+    text: string;
+    status: boolean;
+  }[];
+}
+
+export default defineComponent({
+  components: {
+    AppTodoItem,
+  },
+
+  data(): State {
+    return {
+      items: [
+        { id: 1, text: 'Learn the basics of Vue', status: true },
+        { id: 2, text: 'Learn the basics of Typescript', status: false },
+        { id: 3, text: 'Learn the basics of Nuxt', status: false },
+      ],
+    };
+  },
+});
+</script>
+
 <template>
   <ul class="todo-list">
-    <li class="todo-item todo-item--done">
-      <div class="todo-item__status">
-        <i class="bi bi-check2"></i>
-      </div>
-      <span class="todo-item__text">Learn the basics of Vue</span>
-      <button class="todo-item__remove-button">
-        <i class="bi bi-trash3"></i>
-      </button>
-    </li>
-    <li class="todo-item">
-      <div class="todo-item__status">
-        <i class="bi bi-check2"></i>
-      </div>
-      <span class="todo-item__text">Learn the basics of Typescript</span>
-      <button class="todo-item__remove-button">
-        <i class="bi bi-trash3"></i>
-      </button>
-    </li>
-    <li class="todo-item">
-      <div class="todo-item__status">
-        <i class="bi bi-check2"></i>
-      </div>
-      <span class="todo-item__text">Subscribe to the channel</span>
-      <button class="todo-item__remove-button">
-        <i class="bi bi-trash3"></i>
-      </button>
-    </li>
+    <AppTodoItem v-for="item in items" :key="item.id" :item="item" />
   </ul>
 </template>
